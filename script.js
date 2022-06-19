122ceae2c4bf33b68f5bfa3dc51e4c06151b8b6a
Carlos Reges <kerg23@gmail.com>
2022-06-19 12:16:31 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function triangleTwo() {
+       
+    for(let i = 1; i <= 6; i++){
+        for (let j = 1; j <= i; j++) {
+            document.write(j);
+            console.log(j);
+        }
+        console.log("<br>");
+        document.write("<br>");
+    }
+}
