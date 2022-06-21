13a0384bf831db584304dac5bc67b587f34136e1
Carlos Reges <kerg23@gmail.com>
2022-06-21 16:22:55 -0300

    Update script.js
    Arreglando línea de sobra

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function triangleTwo() {
-    for(let i = 1; i <= 6; i++){
-        for (let j = 1; j <= i; j++) {
-            document.write(j);
-            console.log(j);
-        }
-        console.log("<br>");
-        document.write("<br>");
-    }
+  for(let i = 1; i <= 6; i++){
+      for (let j = 1; j <= i; j++) {
+          document.write(j);
+          console.log(j);
+      }
+      document.write("<br>");
+  }
 }
 triangleTwo();
